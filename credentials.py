d65bd46aa540e787159be9ffd80eab281ad43d1d
PharmakonSpace <pharmakonspace@gmail.com>
2025-05-14 13:10:01 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,10 @@
+#replace  username,pwd, api_key  with yours
+USER_NAME = 'T51480206'
+PWD = '0322'
+API_KEY = 'X7i4TSWn'
+TOTP_SECRET="WDCTRUBZB2HCKSVCUB6L4FDJFI"
+
+#Dont change  below varibale 
+FEED_TOKEN = None
+TOKEN_MAP = None
+SMART_API_OBJ = None
