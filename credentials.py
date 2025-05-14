4cd2e6ed98e123e2b6994e2cd3e20e65b60d2b59
PharmakonSpace <pharmakonspace@gmail.com>
2025-05-14 13:25:33 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,8 +1,8 @@
 #replace  username,pwd, api_key  with yours
-USER_NAME = 'T51480206'
-PWD = '0322'
-API_KEY = 'X7i4TSWn'
-TOTP_SECRET="WDCTRUBZB2HCKSVCUB6L4FDJFI"
+USER_NAME = 'T^@480$%#'
+PWD = '567489'
+API_KEY = 'X7i4FRHn'
+TOTP_SECRET="WDCTRUBZB2HCKSVCUB6L4FDDFRE"
 
 #Dont change  below varibale 
 FEED_TOKEN = None
